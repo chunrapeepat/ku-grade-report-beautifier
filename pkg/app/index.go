72dc4ef17fde8684f28072c261e43e194db425f9
chunza2542 <chunza2542@hotmail.com>
2017-12-27 15:46:49 +0700

    check 404 not gound

--- pkg/app/index.go
+++ pkg/app/index.go
@@ -3,5 +3,9 @@ package app
 import "net/http"
 
 func indexGetHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Welcome to index page"))
 }
