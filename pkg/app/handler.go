0a03e5b2a7e531a441a72ac30a8f46f7ac4901be
chunza2542 <chunza2542@hotmail.com>
2017-12-29 11:29:49 +0700

    render user information to view

--- pkg/app/handler.go
+++ pkg/app/handler.go
@@ -3,14 +3,43 @@ package app
 import (
 	"net/http"
 
+	"github.com/chunza2542/ku-grade-report-beautifier/pkg/model"
 	"github.com/chunza2542/ku-grade-report-beautifier/pkg/view"
 )
 
 func indexGetHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
+	if r.Method == http.MethodGet {
+		view.Index(w, r, &view.IndexData{})
+	} else {
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+		// Login to endpoint using Nontri account
+		session, err := model.Login(username, password, "0")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		// Get User information & Grade
+		userInfo, err := model.GetUserInformation(session)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		grade, err := model.GetGradeReport(session)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		// Logout & Destroy Session
+		err = model.Logout(session)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		// Parse Data
+		view.Report(w, r, &view.ReportData{
+			UserInfo:   userInfo,
+			CourseInfo: grade,
+		})
 	}
-	// view.Index(w, r, &view.IndexData{})
-	view.Report(w, r, &view.ReportData{})
 }
--- pkg/view/view.go
+++ pkg/view/view.go
@@ -1,17 +1,27 @@
 package view
 
-import "net/http"
+import (
+	"net/http"
 
+	"github.com/chunza2542/ku-grade-report-beautifier/pkg/api"
+)
+
+// IndexData view
 type IndexData struct {
 }
 
+// ReportData view
 type ReportData struct {
+	UserInfo   *api.UserInfo
+	CourseInfo []*api.CourseInfo
 }
 
+// Index render index template to view
 func Index(w http.ResponseWriter, r *http.Request, data *IndexData) {
 	render(tpIndex, w, data)
 }
 
+// Report render report template to view
 func Report(w http.ResponseWriter, r *http.Request, data *ReportData) {
 	render(tpReport, w, data)
 }
